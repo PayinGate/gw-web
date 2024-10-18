946c4ce31b18c90e362cc3b7cc3b91310d6890cd
WilAy <ayomikunakintade@gmail.com>
2024-10-18 23:54:18 +0100

    update

--- src/pages/dashboard/transactions/view_transaction.js
+++ src/pages/dashboard/transactions/view_transaction.js
@@ -20,13 +20,25 @@ export default function ViewTransaction(){
     }, [params.id])
 
     return (<>
-        <div className="p-2 h-screen">
+        <div className="p-2 h-screen flex flex-col gap-3">
             <div className="flex items-center gap-2">
                 <span className={classNames(`h-[8px] w-[8px] rounded-full tr1_status ${transaction?.status}`)}></span>
                 <span className="font-Archivo text-[18px] font-bold">Transaction</span>
-                <span className="font-medium text-[12px] bg-[#d4d4d49f] dark:bg-[#3434349f] py-1 px-2 rounded-lg text-[#828282] dark:text-[#bbbbbb] font-inter cursor-pointer">{transaction?.reference}</span></div>
+                <span className="font-medium text-[12px] bg-[#d4d4d49f] dark:bg-[#3434349f] py-1 px-2 rounded-lg text-[#828282] dark:text-[#bbbbbb] font-inter cursor-pointer">{transaction?.reference}</span>
+            </div>
+            <div className="flex w-full gap-3">
+                <div className="w-full"></div>
+                <div className="w-full flex flex-col">
+                    <div className="w-full h-full">
+                        chain
+                    </div>
+                    <div className="w-full h-full">
+                        logs
+                    </div>
+                </div>
+            </div>
         </div>
-        <pre className="bg-[#282C34] text-[#CCCCCC] text-[14px] p-4 rounded-[5px] overflow-x-auto" style={{boxShadow: "0 4px 6px rgba(0, 0, 0, 0.1)"}}>
+        <pre className="bg-[#282C34] text-[#CCCCCC] text-[14px] p-4 rounded-[5px] overflow-x-auto mb-24" style={{boxShadow: "0 4px 6px rgba(0, 0, 0, 0.1)"}}>
             <code>
                 {JSON.stringify(transaction, null, 2)}
             </code>
