cce717bec95d395f529ab34fd6ffb50cd72d03bf
WilAy <ayomikunakintade@gmail.com>
2025-12-18 23:17:43 +0100

    fix: error handling

--- src/api/handler.js
+++ src/api/handler.js
@@ -3,7 +3,7 @@ import { getCookie } from '../utils/utils';
 
 class API {
   constructor(baseURL) {
-    this.baseURL = window.location.hostname === "localhost" ? "http://localhost:3000" :'https://2fe99342f6c6.ngrok-free.app';
+    this.baseURL = window.location.hostname === "localhost" ? "http://localhost:3000" :'https://854e1699b12a.ngrok-free.app';
     this.get = this.get.bind(this);
     this.post = this.post.bind(this);
   }
