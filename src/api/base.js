7c026748f43abd16f5d01b2104765227f26a62f6
WilAy <ayomikunakintade@gmail.com>
2026-01-06 17:18:23 +0100

    online update

--- src/api/base.js
+++ src/api/base.js
@@ -1,6 +1,8 @@
 let BASE_URL; 
 
-if(document.location.hostname === "localhost") BASE_URL = process.env.REACT_APP_API_URL;
-else BASE_URL = process.env.REACT_APP_IP_URL;
+// if(document.location.hostname === "localhost") BASE_URL = process.env.REACT_APP_API_URL;
+// else BASE_URL = process.env.REACT_APP_IP_URL;
+
+BASE_URL = "https://gateway-riuk.onrender.com";
 
 export { BASE_URL as baseUrl };
